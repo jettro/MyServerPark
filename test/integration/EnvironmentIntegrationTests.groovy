a7add357c1ba9a73cf48e10ceb2350156e85e781
jettro <jettro@gridshore.nl>
2009-11-22 21:15:30 +0100

    made the integration test for environment and ordering work

--- test/integration/EnvironmentIntegrationTests.groovy
+++ test/integration/EnvironmentIntegrationTests.groovy
@@ -6,23 +6,23 @@ class EnvironmentIntegrationTests extends GrailsUnitTestCase {
 
     protected void setUp() {
         env = new Environment(name:'dev',lastDeploy: new DateTime()).save()
-        env.addToLinks new Link(title:'btest',url:'http://test.gridshore.nl')
+        env.addToLinks new Link(title:'bTest',url:'http://test.gridshore.nl')
         env.addToLinks new Link(title:'aTest',url:'http://atest.gridshore.nl')
         env.addToLinks new Link(title:'zTest',url:'http://ztest.gridshore.nl')
         env.addToLinks new Link(title:'tTest',url:'http://ztest.gridshore.nl')
+        env.save(flush:true)
     }
-
-
 
     void testEnvironmentLinkSorting() {
         def foundEnv = Environment.get(env.id)
+        foundEnv.refresh()
         def links = foundEnv.links.collect {it.title}
 
         assertEquals(['aTest','bTest','tTest','zTest'],links)
     }
 
     void testLinkSorting() {
-        def foundLinks = Link.findAll()
+        def foundLinks = Link.list()
         assertEquals(['aTest','bTest','tTest','zTest'],foundLinks.collect {it.title})        
     }
 }
